0b564c400bd66abb51641686fa063384ec9e7926
Padmaashini Sukumaran <padmaashini.s@gmail.com>
2022-04-22 10:51:16 -0400

    fixed database url

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://padma:Yellow200!@grocerycart.cqiciimjj0vv.ca-central-1.rds.amazonaws.com/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://padma:Yellow200!@grocerycart.cqiciimjj0vv.ca-central-1.rds.amazonaws.com/grocerycart"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
